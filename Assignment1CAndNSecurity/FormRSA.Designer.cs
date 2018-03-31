d01942404fe1e8c0ed8194f569bcc75012389a2c
Hien <1411231@hcmut.edu.vn>
2018-03-31 11:00:21 +0700

    Form RSA (not complete)

--- Assignment1CAndNSecurity/FormRSA.Designer.cs
+++ Assignment1CAndNSecurity/FormRSA.Designer.cs
@@ -52,6 +52,7 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.btnReset)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnDecrypt)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnEncrypt)).BeginInit();
@@ -158,7 +159,7 @@
             this.btnEncrypt.BackColor = System.Drawing.Color.Transparent;
             this.btnEncrypt.BackgroundImage = global::Assignment1CAndNSecurity.Properties.Resources.encrypt;
             this.btnEncrypt.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnEncrypt.Location = new System.Drawing.Point(147, 467);
+            this.btnEncrypt.Location = new System.Drawing.Point(157, 467);
             this.btnEncrypt.Name = "btnEncrypt";
             this.btnEncrypt.Size = new System.Drawing.Size(231, 117);
             this.btnEncrypt.TabIndex = 23;
@@ -173,7 +174,7 @@
             this.btnOutOpen.BackColor = System.Drawing.Color.Transparent;
             this.btnOutOpen.BackgroundImage = global::Assignment1CAndNSecurity.Properties.Resources.OPEN;
             this.btnOutOpen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnOutOpen.Location = new System.Drawing.Point(648, 412);
+            this.btnOutOpen.Location = new System.Drawing.Point(662, 412);
             this.btnOutOpen.Name = "btnOutOpen";
             this.btnOutOpen.Size = new System.Drawing.Size(86, 44);
             this.btnOutOpen.TabIndex = 22;
@@ -188,7 +189,7 @@
             this.btnOpenFolderIn.BackColor = System.Drawing.Color.Transparent;
             this.btnOpenFolderIn.BackgroundImage = global::Assignment1CAndNSecurity.Properties.Resources.Open_folder;
             this.btnOpenFolderIn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnOpenFolderIn.Location = new System.Drawing.Point(739, 347);
+            this.btnOpenFolderIn.Location = new System.Drawing.Point(774, 362);
             this.btnOpenFolderIn.Name = "btnOpenFolderIn";
             this.btnOpenFolderIn.Size = new System.Drawing.Size(86, 44);
             this.btnOpenFolderIn.TabIndex = 19;
@@ -203,7 +204,7 @@
             this.btnOpenFileIn.BackColor = System.Drawing.Color.Transparent;
             this.btnOpenFileIn.BackgroundImage = global::Assignment1CAndNSecurity.Properties.Resources.Open_file;
             this.btnOpenFileIn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnOpenFileIn.Location = new System.Drawing.Point(648, 362);
+            this.btnOpenFileIn.Location = new System.Drawing.Point(662, 362);
             this.btnOpenFileIn.Name = "btnOpenFileIn";
             this.btnOpenFileIn.Size = new System.Drawing.Size(86, 44);
             this.btnOpenFileIn.TabIndex = 18;
@@ -322,11 +323,22 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(299, 70);
+            this.label7.Font = new System.Drawing.Font("Papyrus", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(80, 50);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.Size = new System.Drawing.Size(220, 100);
             this.label7.TabIndex = 34;
-            this.label7.Text = "label7";
+            this.label7.Text = "RSA";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Papyrus", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(292, 89);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(136, 42);
+            this.label8.TabIndex = 35;
+            this.label8.Text = "Algorithm";
             // 
             // FormRSA
             // 
@@ -334,6 +346,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1054, 651);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -405,5 +418,6 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
